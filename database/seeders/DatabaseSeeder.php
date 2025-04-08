2a78fed0963e3c75566448e0c20bbd08a1f1411e
Tarawns <tarawanesha@gmail.com>
2025-04-08 19:05:15 +0700

    seeding dan n + 1 problem

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,15 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use Database\Factories\CategoryFactory;
+use GuzzleHttp\Promise\Create;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +19,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        //User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // Category::create([
+        //     'name' => 'Web Desain',
+        //     'slug' => 'web-desain'
+        // ]);
+
+        // Post::create([
+        //     'title' => 'judul artikel 1',
+        //     'author_id' => 1,
+        //     'category_id' => 1,
+        //     'slug' => 'judul-artikel-1',
+        //     'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit repudiandae, minus sint aperiam, cumque laboriosam amet officia autem hic asperiores tempore nam corrupti reprehenderit non sequi, ut cum maxime numquam.'
+        // ]);
+
+
+        $this->call([CategorySeeder::class, UserSeeder::class]);
+        Post::factory(100)->recycle([
+            Category::all(),
+            User::all()        
+        ])->create();
     }
 }
